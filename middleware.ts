d79b5e291d3987946289938e3320e2c5c0d91012
abdelouahed <abdelouahed4@yahoo.fr>
2023-05-28 19:16:48 +0100

    change1

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-export { default } from "next-auth/middleware"
-//middleware  used to authenticate requests for the paths "/trips", "/reservations", "/properties", and "/favorites"
-export const config = { 
-  matcher: [
-    "/comments",
-    "/feedbackclient",
+// export { default } from "next-auth/middleware"
+// //middleware  used to authenticate requests for the paths "/trips", "/reservations", "/properties", and "/favorites"
+// export const config = { 
+//   matcher: [
+//     "/comments",
+//     "/feedbackclient",
 
-  ]
-};
+//   ]
+// };
